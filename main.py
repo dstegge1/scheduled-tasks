0d427c4e27ad0b427d2d322406c6c28ff79113d9
dstegge1 <dylansteggerda@gmail.com>
2026-05-19 14:21:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import smtplib
 import os
 
-weather_api_key = os.environ["WEATHER_API_KEY"]
-my_email = os.environ["MY_EMAIL"]
-to_email = os.environ["TO_EMAIL"]
-my_password = os.environ["MY_PASSWORD"]
+weather_api_key = os.getenv("WEATHER_API_KEY")
+my_email = os.getenv("MY_EMAIL")
+to_email = os.getenv("TO_EMAIL")
+my_password = os.getenv("MY_PASSWORD")
 
 url = "https://api.openweathermap.org/data/2.5/forecast"
 browser_url = "https://api.openweathermap.org/data/2.5/forecast?lat=39.29&lon=76.61&appid=5e87e0cbf287e67a6e62a6ec4d19c854"
